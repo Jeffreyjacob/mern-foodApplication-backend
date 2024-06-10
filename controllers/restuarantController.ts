7838d63f8b3ec9a952d554b5b275f8bd8efde9ea
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-06-10 01:51:18 -0700

    new commit

--- controllers/restuarantController.ts
+++ controllers/restuarantController.ts
@@ -7,7 +7,7 @@ const searchRestuarant = async (req:Request,res:Response)=>{
   try{
     const city = req.params.city;
     const searchQuery = (req.query.searchQuery as string) || "";
-    const selectedCuisines = (req.query.sekectedCuisines as string) || "";
+    const selectedCuisines = (req.query.selectedCuisines as string) || "";
     const sortOption = (req.query.sortOption as string) || "lastUpdated";
     const page = parseInt(req.query.page as string) || 1
     
